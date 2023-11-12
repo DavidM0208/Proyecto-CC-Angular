572b8293b4f463493d6f261870473ba32c712e83
Carlosbravo16 <110642356+Carlosbravo16@users.noreply.github.com>
2023-11-12 13:25:16 -0500

    se adelanto la parte del front

--- cencoe/src/app/app.module.ts
+++ cencoe/src/app/app.module.ts
@@ -5,17 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { CampaignComponent } from './view/campaign/campaign.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    CampaignComponent
+    CampaignComponent,
+    DashboardComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
